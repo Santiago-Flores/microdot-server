6af30090a890319e36c3beed30a431e19218e2b9
Santiago-Flores <santiflo06@gmail.com>
2024-11-26 12:56:28 -0300

    feat: agrego boot.py

--- actividades/ej_01/boot.py
+++ actividades/ej_01/boot.py
@@ -1,1 +1,13 @@
 # Configuracion inicial
+def initialize_connection():
+    import network
+    wifi = network.WLAN(network.STA_IF)
+    if not wifi.isconnected():
+        print('Estableciendo conexión con el servidor...')
+        wifi.active(True)
+        wifi.connect('Cooperadora Alumnos', '')
+        while not wifi.isconnected():
+            pass
+    print('Configuración de red:', wifi.ifconfig())
+
+initialize_connection()
