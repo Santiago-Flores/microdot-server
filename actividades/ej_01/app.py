49566ad147ef64d7a7ea95928d2115b0a0c092c9
Santiago-Flores <santiflo06@gmail.com>
2024-11-26 12:42:28 -0300

    feat: Agregar app.py de programa que muestra texto centrado que contenga algún mensaje

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,17 @@
 # Aplicacion del servidor
+from boot import initialize_connection
+from microdot import Microdot, send_file
+
+initialize_connection()
+application = Microdot()
+
+@application.route('/')
+async def home(request):
+    return send_file('index.html')
+
+@application.route('/<folder>/<filename>')
+async def serve_file(request, folder, filename):
+    return send_file(f"/{folder}/{filename}")
+
+application.run(port=80)
+
